eb2440ca47d4131319de1aec0a023efb0bac764f
Souvik Dey <Souvik.Dey@thomsonreuters.com>
2020-05-27 11:53:29 +0530

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RedComponent } from './components/red/red.component';
+import { BlueComponent } from './components/blue/blue.component';
+import { GreenComponent } from './components/green/green.component';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'red', component: RedComponent},
+  {path: 'blue', component: BlueComponent},
+  {path: 'green', component: GreenComponent},
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RedComponent } from './components/red/red.component';
+import { BlueComponent } from './components/blue/blue.component';
+import { GreenComponent } from './components/green/green.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedComponent,
+    BlueComponent,
+    GreenComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
