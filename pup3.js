aa22cf1da56c0a87f68f57c0653634a9524c92ad
Souvik Dey <Souvik.Dey@thomsonreuters.com>
2020-05-29 21:39:08 +0530

    COmmit

--- pup3.js
+++ pup3.js
@@ -1,21 +1,22 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
-async function run() {
+async function run(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://localhost:4200/');
+    await page.goto(`http://localhost:4200${url}`);
     let bodyHTML = await page.content();//page.evaluate(() => document.body.innerHTML);
     browser.close();
     return bodyHTML;
 }
 const app = express();
 app.get('*', (req, res) => {
-    run().then((html) => {
+    console.log(req.url)
+    run(req.url).then((html) => {
         res.send(html);
     })
 
 });
-app.listen(4000, () => {
-    console.log(`Node Express server listening on http://localhost:4000`);
+app.listen(3000, () => {
+    console.log(`Node Express server listening on http://localhost:3000`);
 });
 // run();
